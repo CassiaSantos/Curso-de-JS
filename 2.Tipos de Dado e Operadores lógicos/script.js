fccc3806cdf2cbd7a723a3d56dec3c5b070deae4
CassiaSantos <cassia.oliveira.profissional@gmail.com>
2022-03-20 18:35:38 -0300

    Tipos de dados em JS

---
+++ 2.Tipos de Dado e Operadores lógicos/script.js
@@ -0,0 +1,30 @@
+// NUMBER
+
+console.log(typeof 13);
+console.log(typeof -8.5);
+console.log(typeof 130.5);
+
+/*
+Operadores Aritméticos:
++ soma
+- subtração
+/ divisão
+* multiplicação
+% resto
+*/
+
+/*
+Especial Numbers:
+- são considerados números mas não são números:
+Infinity --> infinito;
+-Infinity --> menos infinito;
+NaN (Not a Number) --> operação entre number com não number */
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+
+// STRINGS
+console.log(typeof "Cássia Santos");
+console.log(typeof 'Dev');
+console.log(typeof `Fullstack`);
